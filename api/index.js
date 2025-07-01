03da7cd3a601b6eb144b1d320b71c86ec6f14cb1
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2025-07-01 17:45:19 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() =>
+    console.log('Connected to MongoDB')
+)
+.catch(err =>
+    console.error('Error connecting to MongoDB:', err)
+);
 
 const app = express();
 app.listen(3000, () => {
