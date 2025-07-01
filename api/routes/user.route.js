82b35b345c0996ce3b8816d8a02c8d27137951c6
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2025-07-01 18:28:09 +0530

    test api route

---
+++ api/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { test } from '../controllers/user.controller.js'; // Adjust the path as necessary
+const router = express.Router();
+router.get('/test', test);
+
+
+
+export default router;
