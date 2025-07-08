fec2f4c77e49c2b9cdc317eb5cbdb0c49148b5db
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2025-07-08 21:30:08 +0530

    complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
+import { errorHander } from './error.js';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
     if (!token) {
-        return next(nextHandler(401, 'You are not authenticated'));
+        return next(errorHandler(401, 'You are not authenticated'));
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            return next(nextHandler(403, 'Token is not valid'));
+            return next(errorHander(403, 'Token is not valid'));
         }
         req.user = user;
         next();
