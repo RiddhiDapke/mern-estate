284b4ac0feabfdfa00bd175d91e76b6ce351b22c
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2025-07-14 11:56:43 +0530

    complete upload listing image functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-d5abf.firebaseapp.com",
-  projectId: "mern-estate-d5abf",
-  storageBucket: "mern-estate-d5abf.firebasestorage.app",
-  messagingSenderId: "372500880719",
-  appId: "1:372500880719:web:50244fda4ac4ca4487b93a"
+  authDomain: "estate-project-5ff47.firebaseapp.com",
+  projectId: "estate-project-5ff47",
+  storageBucket: "estate-project-5ff47.firebasestorage.app",
+  messagingSenderId: "754687381060",
+  appId: "1:754687381060:web:5e7837c3b2e59b1cf5b611"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
