3ecc83bf04a3de7fdb546b54893b856773ace950
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2025-07-15 23:02:41 +0530

    create update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -33,7 +33,26 @@ export const deleteListing = async(req, res , next) =>{
     }
 };
 
-export const updateListing = async (req, res, next) =>{
+// export const updateListing = async (req, res, next) =>{
+//     const listing = await Listing.findById(req.params.id);
+//     if(!listing){
+//         return next(errorHander(404, 'Listing not found'));
+//     }
+    
+//     if(req.user.id !== listing.userRef){
+//         return next(errorHander(403, 'You are not authorized to update this listing'))
+//     }
+//     try {
+//         const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new: true});
+//         res.status(200).json(updatedListing);
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+
+
+export const updateListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
     if(!listing){
         return next(errorHander(404, 'Listing not found'));
@@ -44,8 +63,29 @@ export const updateListing = async (req, res, next) =>{
     }
     try {
         const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new: true});
-        res.status(200).json(updatedListing);
+        res.status(200).json({
+            success: true,
+            listing: updatedListing
+        });
     } catch (error) {
         next(error);
     }
 };
+
+export const getListing = async (req, res, next) =>{
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if(!listing){
+            return next(errorHander(404, 'Listing not found'));
+        }
+        res.status(200).json({
+            success: true,
+            listing,
+        });
+        // res.status(200).json(listing);
+            
+            
+    } catch (error) {
+        next(error);
+    }
+}
