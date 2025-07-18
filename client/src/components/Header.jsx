e3e5c79f4592e6c2b4785150f1910b7b650e036f
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2025-07-18 22:04:21 +0530

    complete about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,84 +1,3 @@
-// import React from 'react'
-// import { Link, useNavigate, useLocation} from 'react-router-dom'
-// import { FaSearch} from 'react-icons/fa'
-// import { useSelector } from 'react-redux'
-// import { useState, useEffect } from 'react'
-
-
-// export default function Header() {
-//   const navigate = useNavigate();
-//   const location = useLocation();
-//     const {currentUser} = useSelector((state) => state.user);
-//     const [searchTerm, setSearchTerm] = useState('');
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const urlParams = new URLSearchParams(location.search);
-//     urlParams.set('searchTerm', searchTerm);
-//     const searchQuery = urlParams.toString();
-//     navigate(`/search?${searchQuery}`);
-//     console.log('Search Term:', searchTerm);
-
-
-//   }
-
-//   useEffect(() => {
-//     const urlParams = new URLSearchParams(location.search);
-//     const searchTermFromUrl = urlParams.get('searchTerm');
-//     if (searchTermFromUrl) {
-//       setSearchTerm(searchTermFromUrl);
-//     }
-//   }, [location.search]);
-
-//   return (
-//     <header className='flex justify-between items-center p-2 bg-slate-200 shadow-md mx-auto max-w-6xl'>
-//         <Link to="/" >
-//       <h1 className='font-bold test-sm sm:text-xl flex flex-wrap'>
-//         <span className='text-slate-500'>Sahand</span>
-//         <span className='text-slate-700' >Estate</span>
-//       </h1>
-//       </Link>
-//       <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center sm:w-auto'>
-//         <input
-//           type="text"
-//           placeholder="Search..."
-//           className="bg-transparent focus:outline-none w-24 sm:w-64"
-//           value={searchTerm}
-//           onChange={(e) => setSearchTerm(e.target.value)}
-          
-
-//         />
-//         <button>
-//           <FaSearch className='text-slate-600'/>
-//         </button>
-        
-
-//       </form>
-//       <ul className='flex  gap-4 text-sm sm:text-base'>
-//         <Link to="/">
-//         <li className='hidden sm:inline text-slate-700 hover:underline'>
-//             Home
-//         </li>
-//         </Link>
-        
-//         <Link to="/about">
-//         <li className='hidden sm:inline text-slate-700 hover:underline'>
-//             About
-//         </li>
-//         </Link>
-        
-//         <Link to="/profile">
-//         {currentUser ? (
-//             <img src={currentUser.avatar} alt="profile" className='w-7 h-7 rounded-full object-cover' />
-//         ): <li className=' text-slate-700 hover:underline'>
-//             Sign In
-//         </li>
-//         }        
-//         </Link>
-//       </ul>
-//     </header>
-//   )
-// }
-
 
 import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
@@ -109,7 +28,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-500'>Diamond</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
